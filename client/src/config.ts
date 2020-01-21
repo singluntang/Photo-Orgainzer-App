cd29e379ecfe1691d1508733b78fa36b48189cbb
singlun <programmingwebsite@outlook.com>
2020-01-21 20:46:49 +0800

    Feat: Config file updated

--- client/src/config.ts
+++ client/src/config.ts
@@ -8,8 +8,8 @@ export const stage = 'prod'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/${stage}`
 
 export const authConfig = {
-  domain: 'dev-rs5nalxx.auth0.com',
-  clientId: 'FzUNMm5CZ3rqAwFoncs6FePJvkRMLbk0',
+  domain: '------',
+  clientId: '------',
   callbackUrl: `http://localhost:3000/callback`
 }
 
